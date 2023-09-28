350a47b6fe2fb7aa26836e6ba097a6e3646a786f
Yusuf-R <y.abdulwasiu@gmail.com>
2023-09-28 04:17:29 +0100

    feat: :sparkles: only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,14 @@
+//Keep every promise you make and only make promises you can keep
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    /* eslint-disable */
+      if (true) {
+        resolve();
+      } else {
+        reject();
+      }
+      /* eslint-enable */
+  });
+}
+
+export default getResponseFromAPI;
