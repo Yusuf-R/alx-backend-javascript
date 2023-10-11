b6924b7c6aafde3112f5483f3a9feb9ee592ca12
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-11 06:55:34 +0100

    feat: :sparkles: handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,30 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+async function handleProfileSignup(firstName, lastName, filename) {
+  try {
+    const photoPromise = await uploadPhoto(filename);
+    const userPromise = await signUpUser(firstName, lastName);
+
+    const [photo, user] = await Promise.all([photoPromise, userPromise]);
+    return [
+      {
+        status: 'success',
+        value: user,
+      },
+      {
+        status: 'success',
+        value: photo,
+      },
+    ];
+  } catch (err) {
+    return [
+      {
+        status: 'error',
+        value: err,
+      },
+    ];
+  }
 }
 
 export default handleProfileSignup;
