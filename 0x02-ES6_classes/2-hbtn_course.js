2cfc832fe6c294bdb5c743a20a627841ba0078e0
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-17 20:36:34 +0100

    feat: :sparkles: fix bugs

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,26 +1,26 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
   constructor(name, length, students) {
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  set name(name) {
-    if (typeof name !== 'string') {
+  set name(value) {
+    if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    this._name = name;
+    this._name = value;
   }
 
   get name() {
     return this._name;
   }
 
-  set length(length) {
-    if (typeof length !== 'number') {
+  set length(value) {
+    if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    this._length = length;
+    this._length = value;
   }
 
   get length() {
@@ -31,17 +31,16 @@ class HolbertonCourse {
     return this._students;
   }
 
-  set students(newStudents) {
-    if (!(newStudents instanceof Array)) {
+  set students(currStudents) {
+    if (typeof currStudents === 'object') {
+      for (const student in currStudents) {
+        if (typeof student !== 'string') {
+          throw new TypeError('Students must be an array of strings');
+        }
+      }
+    } else {
       throw new TypeError('Students must be an array of strings');
     }
-
-    if (!newStudents.every((newStudent) => typeof newStudent === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
-
-    this._students = newStudents;
+    this._students = currStudents;
   }
 }
-
-export default HolbertonCourse
