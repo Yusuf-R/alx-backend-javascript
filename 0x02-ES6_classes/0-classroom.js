61732d8511aff3f2134c47d2c74f5e843e74d906
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-17 03:46:53 +0100

    feat: :sparkles: implement a class name with a defualt value

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,9 +1,7 @@
-/* Classroom
-    https://stackoverflow.com/questions/4484424/underscore-prefix-for-property-and-method-names-in-javascript
-    prefix
-*/
-export default class ClassRoom {
-  constructor(maxStudentsSize = 0) {
+class Classroom  {
+  constructor (maxStudentsSize=0) {
     this._maxStudentsSize = maxStudentsSize;
   }
 }
+
+export default Classroom
