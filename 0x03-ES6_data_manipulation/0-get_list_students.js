98ab96da7716d6723f2b70ee278059f66ec89030
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-18 22:39:15 +0100

    feat: :sparkles: mystyle

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,29 @@
-// Get List Students
+// Create a function named getListStudents that returns an array of objects.
 
-const getListStudents = () => {
-  const students = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+// Each object should have three attributes: id (Number), firstName (String), and location (String).
+
+// The array contains the following students in order:
+
+// Guillaume, id: 1, in San Francisco
+// James, id: 2, in Columbia
+// Serena, id: 5, in San Francisco
+
+export default function getListStudents() {
+  return [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
   ];
-
-  return students;
-};
-
-export default getListStudents;
+}
