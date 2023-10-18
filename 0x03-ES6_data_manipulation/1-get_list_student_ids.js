e597cbef1e2dcf95e9372e8c941fa5620a4829af
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-18 23:04:44 +0100

    feat: :sparkles: map filter application

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,12 +1,16 @@
-// List students
+// Create a function getListStudentIds that returns an array of ids from a list of object.
 
-const getListStudentIds = (ids) => {
-  if (!Array.isArray(ids)) {
+// This function is taking one argument which is an
+// array of objects - and this array is the same format as getListStudents
+// from the previous task.
+
+// If the argument is not an array, the function is returning an empty array.
+
+// You must use the map function on the array.
+
+export default function getListStudentIds(array) {
+  if (!Array.isArray(array)) {
     return [];
   }
-  const meids = ids.map((item) => item.id);
-
-  return meids;
-};
-
-export default getListStudentIds;
+  return array.map((obj) => (obj.id));
+}
