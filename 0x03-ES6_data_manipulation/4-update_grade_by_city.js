332fa57b9ff2207fe90fe8f1fe3461228d2da707
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-19 02:03:55 +0100

    feat: :sparkles: filter and update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,24 +1,27 @@
-// Grade by city and add grade
+/* eslint-disable brace-style */
+/* eslint-disable no-param-reassign */
+// Create a function updateStudentGradeByCity that returns an array of students
+// for a specific city with their new grade
 
-const updateStudentGradeByCity = (students, city, newGrades) => {
-  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+// It should accept a list of students (from getListStudents), a city (String),
+// and newGrades (Array of “grade” objects) as parameters.
+
+// newGrades is an array of objects with this format:
+
+export default function updateStudentGradeByCity(array, city, newGrades) {
+  if (!Array.isArray(array)) {
     return [];
   }
-
-  const studentByCity = students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const grades = newGrades.filter((note) => student.id === note.studentId);
-      let grade = 'N/A';
-
-      if (grades[0]) {
-        grade = grades[0].grade;
+  return array
+    .filter((x) => x.location === city)
+    .map((x) => {
+      const newGrade = newGrades.find((y) => y.studentId === x.id);
+      if (newGrade) {
+        x.grade = newGrade.grade;
       }
-
-      return { ...student, grade };
+      // If no grade was found, set the grade to “N/A”
+      else if (!newGrade) {
+        x.grade = 'N/A';
+      } return x; // Return the modified student object
     });
-
-  return studentByCity;
-};
-
-export default updateStudentGradeByCity;
+}
