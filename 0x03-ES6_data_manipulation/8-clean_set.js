eeac7ecec168c0b61e74ac70c206ebcb2bee497e
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-19 08:52:22 +0100

    feat: :sparkles: clear set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,23 +1,30 @@
-// Clean set with strings coincidences
+// Create a function named cleanSet that returns a string of all
+// the set values that start with a specific string (startString).
 
-const cleanSet = (set, startString) => {
-  const str = [];
+// It accepts two arguments: a set (Set) and a startString (String).
 
-  if (
-    typeof set !== 'object'
-    || typeof startString !== 'string'
-    || startString.length === 0
-  ) {
+// When a value starts with startString you only append the rest of
+// the string. The string contains all the values of the set separated by -.
+
+export default function cleanSet(set, startString) {
+  if (!(set instanceof Set)) {
     return '';
   }
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
+  }
+  let res = ''; // Initialize an empty string to store the result
 
-  for (const item of set) {
-    if (item && item.startsWith(startString)) {
-      str.push(item.slice(startString.length));
+  for (let x of set) {
+    if (typeof x === 'string' && x.startsWith(startString)) {
+      // Check if x is a string and starts with the specified string
+      x = x.slice(startString.length); // Remove the starting string from x
+      res = `${res}${x}-`; // Concatenate x to the result string with a dash
     }
   }
+  if (res.length > 0) {
+    res = res.slice(0, -1); // Remove the trailing dash if there are elements in the result
+  }
 
-  return str.join('-');
-};
-
-export default cleanSet;
+  return res;
+}
