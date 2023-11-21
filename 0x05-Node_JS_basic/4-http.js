7ae2d9b50ea12f71299f845c4bebda94642b2b0e
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-21 17:13:51 +0100

    feat: :sparkles: a small HTTP server using node HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,20 @@
+// , create a small HTTP server using the http module:
+
+// It should be assigned to the variable app and this one must be exported
+// HTTP server should listen on port 1245
+// Displays Hello Holberton School! in the page body for any endpoint as plain text
+
+const http = require('node:http');
+
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  const header = {
+    'content-type': 'text/plain',
+  };
+  res.writeHead(200, header);
+  res.end('Hello Holberton School!');
+});
+app.listen(port);
+
+module.exports = app;
