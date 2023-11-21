2553306a4e2f9fa30a613114a07911673b0ddd6d
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-21 13:35:13 +0100

    fix: :bug: dynamically extracting the field_type

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -9,35 +9,40 @@ const { log } = console;
 function countStudents(filePath) {
   try {
     let dataList = [];
-    const sweObj = {};
-    const csObj = {};
+    const studentsByField = {};
+
     const data = fs.readFileSync(filePath, 'utf-8');
     // create a list from the file data
     dataList = data.toString().trim().split('\n');
     // create the list containing the data fields
     const dataFields = dataList[0].split(',');
-    // loop through the data and create an object base on the data fields[3]
+
+    // loop through the data and create an object based on the dynamic data fields
     for (let i = 1; i < dataList.length; i += 1) {
       const cf = dataList[i].trim().split(',');
       const obj = {};
       for (let j = 0; j < dataFields.length; j += 1) {
         obj[dataFields[j]] = cf[j];
       }
-      if (cf[3] === 'SWE') {
-        sweObj[`student_${i}`] = obj;
-      } else if (cf[3] === 'CS') {
-        csObj[`student_${i}`] = obj;
+
+      const { field } = obj;
+      if (!studentsByField[field]) {
+        studentsByField[field] = [];
+      }
+
+      studentsByField[field].push(obj.firstname);
+    }
+
+    // Log the results
+    log(`Number of students: ${dataList.length - 1}`);
+    for (const field in studentsByField) {
+      if (Object.prototype.hasOwnProperty.call(studentsByField, field)) {
+        log(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
       }
     }
-    // create a list of students firstname for both object
-    const csStudents = Object.keys(csObj).map((key) => csObj[key].firstname);
-    const sweStudents = Object.keys(sweObj).map((key) => sweObj[key].firstname);
-
-    log(`Number of students: ${dataList.length - 1}`);
-    log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
-    log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
   } catch (err) {
     throw Error('Cannot load the database');
   }
 }
+
 module.exports = countStudents;
