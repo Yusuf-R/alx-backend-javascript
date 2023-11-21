dc0d44fdd0c7af7b6dbda74df1cbd24cacfa1c9a
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-21 01:24:20 +0100

    feat: :sparkles: uses process for stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,30 @@
+// Create a program named 1-stdin.js that will be executed through command line:
+// It should display the message Welcome to Holberton School,
+// what is your name? (followed by a new line)
+// The user should be able to input their name on a new line
+// The program should display Your name is: INPUT
+// When the user ends the program, it should display This important software is
+// now closing (followed by a new line)
+// Requirements:
+// Your code will be tested through a child process, make sure you have everything you need for that
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+// indicating that input is coming from normal keyboard
+if (process.stdin.isTTY) {
+  process.stdin.on('readable', () => {
+    const name = process.stdin.read();
+    if (name !== null) {
+      process.stdout.write(`Your name is: ${name}`);
+      process.exit();
+    }
+    process.exit();
+  });
+// indicates that the input is coming from a pipe
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => process.stdout.write('This important software is now closing\n'));
+}
