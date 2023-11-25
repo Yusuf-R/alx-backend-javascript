dbecb022e68aadfbea4cc6dffcc7e0c45553de76
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-25 05:17:09 +0100

    fix: :bug: using the done()

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -3,12 +3,14 @@ const assert = require('assert');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('SuiteName: getPaymentTokenFromAPI', () => {
-  it('Validate the getPaymentTokenFromAPI value', async () => {
-    try {
-      const res = await getPaymentTokenFromAPI(true);
-      assert.strictEqual(res.data, 'Successful response from the API');
-    } catch (err) {
-      return err;
-    }
+  it('Validate the getPaymentTokenFromAPI value', (done) => { // Add done as a parameter
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        assert.strictEqual(res.data, 'Successful response from the API');
+        done(); // Invoke done when the asynchronous operation is complete
+      })
+      .catch((error) => {
+        done(error); // Invoke done with an error if there's an issue
+      });
   });
 });
